9fab225dd150873aa11e7df898b5ecbd58fc94f2
diwashtimilsina <126002522+diwashtimilsina@users.noreply.github.com>
2024-01-22 11:52:21 +0545

    Update2 script.sh

--- script.sh
+++ script.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/bash
+echo "Enter root password"
+read pass
 #export PATH=$PATH:/opt/lampp/mysql
 user=root
-pass=9805110993
 echo "ENTER DATABASE NAME"
 read database;
 mysql --user=$user --password=$pass --database=$database -e 'ALTER table student add housenumbeers int(1);' | mysql --user=$user --password=$pass --database=$database -e 'desc student;' | mysql --user=$user --password=$pass --database=$database -e 'drop table student;'| mysql --user=$user --password=$pass --database=$database -e 'desc student;'
